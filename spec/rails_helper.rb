6ac0640de1bbf6ff2390012b2a489ca82b5a3266
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-12 11:24:12 +0800

    Include pundit-matchers in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'pundit/rspec'
+require 'pundit/matchers'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -61,4 +63,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Pundit::Matchers.configure do |config|
+  config.user_alias = :account
 end

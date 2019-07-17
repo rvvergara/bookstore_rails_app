dbc5075bac74d096d8adac3eb1bc47885cc41efb
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-17 15:30:02 +0800

    Remove categories from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: {format: :json } do
     resources :users, param: :username, only: [:create, :update, :show, :index]
     resources :sessions, only: [:create]
-    resources :categories, only: [:create, :update, :destroy]
     resources :books, only: [:index, :create]
   end
 end

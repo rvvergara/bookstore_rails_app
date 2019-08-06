2bcaaaf2980441c4c3f00b6e628982fc1f361593
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-06 20:14:59 +0800

    Show search results count in json response

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -4,6 +4,7 @@ class V1::SearchController < ApplicationController
   def book_search
     page = params[:page] || '1'
     @books = Book.user_book_search(@current_user, params[:q], page)
+    @count = Book.search_by_term(params[:q]).count
     render :books, status: :ok
   end
 end
